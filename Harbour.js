3c7d60a1acdf9eeef016496501deab746e573d56
nivida <samuel.furter@semabit.ch>
2017-08-26 15:30:39 +0200

    Reverted the code back to the old state

--- Harbour.js
+++ Harbour.js
@@ -1,3 +1,9 @@
+
+// Web3
+import Contract from './lib/web3/Contract';
+import Deploy from './lib/web3/Deploy';
+import ConnectionModel from './lib/web3/models/ConnectionModel';
+
 // Harbour
 import Congress from './api/Congress';
 import Version from './api/Version';
@@ -6,7 +12,10 @@ import ContractData from './storage/ContractData'
 
 // Export public modules
 export {
+    Contract,
+    ConnectionModel,
     ContractData,
     Congress,
+    Deploy,
     Version,
 };
