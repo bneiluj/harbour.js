154cae66ed937c2ccd9405b817bd9911d4c1271f
nivida <samuel.furter@semabit.ch>
2017-10-25 20:56:51 +0200

    Comments removed

--- src/harbour/Harbour.js
+++ src/harbour/Harbour.js
@@ -1,17 +1,3 @@
-
-// Web3
-//import Contract from './lib/web3/Contract';
-//import Deploy from './lib/web3/Deploy';
-//import ConnectionModel from './lib/web3/ConnectionModel';
-
-
-// Export public modules
-/*export {
-    Contract,
-    Deploy,
-	ConnectionModel
-};*/
-
 export default class Harbour {
 
 	constructor() {
